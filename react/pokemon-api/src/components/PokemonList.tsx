4ec9397b44ba6b637675075de00b3a4a2eec424d
Carlos Álvarez Vereterra <carlos.vereterra@stemyenergy.com>
2023-12-15 16:37:20 +0100

    Add comments

--- react/pokemon-api/src/components/PokemonList.tsx
+++ react/pokemon-api/src/components/PokemonList.tsx
@@ -6,10 +6,19 @@ interface Pokemon {
   name: string;
 }
 
+// Styling was not in the scope of the test. If you are going to add styling, do it using a layout that makes
+// sense and it's responsive. The layout is weird and not very friendly in mobile devices
+
 const PokemonList: React.FC = () => {
+  // Having two states for pokemonList, one filtered and one unfiltered is a bad practice. Whenever you have "derived" states
+  // from another state, it's best to calculate them at the time of use, not to have them duplicated
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [filteredPokemonList, setFilteredPokemonList] = useState<Pokemon[]>([]);
-  const [valorInput, setValorInput] = useState("");
+  // ??? This state is redundant. You are not using valorInput for anything
+  // const [valorInput, setValorInput] = useState("");
+
+  // Using the right state variables
+  const [query, setQuery] = useState("");
+  const [isSortedAsc, setIsSortedAsc] = useState(false);
 
   useEffect(() => {
     const fetchPokemonList = async () => {
@@ -19,7 +28,6 @@ const PokemonList: React.FC = () => {
           name: pokemon.name,
         }));
         setPokemonList(list);
-        setFilteredPokemonList(list);
       } catch (error) {
         console.error("Error fetching Pokémon list:", error);
       }
@@ -28,20 +36,25 @@ const PokemonList: React.FC = () => {
   }, []);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const query = event.target.value;
-    setValorInput(query);
-    const filteredList = pokemonList.filter((pokemon) =>
-      pokemon.name.toLowerCase().includes(query.toLowerCase())
-    );
-    setFilteredPokemonList(filteredList);
+    setQuery(event.target.value);
   };
 
-  const orderByAlph = () => {
-    const orderedList = [...filteredPokemonList].sort((a, b) =>
-      a.name.localeCompare(b.name)
-    );
-    setFilteredPokemonList(orderedList);
+  const handleSort = () => {
+    setIsSortedAsc(!isSortedAsc);
   };
+
+  // filteredPokemonList is no longer a state variable, it's calculated on each render.
+  // If this were something costly and we had more state variables that trigger re-renders
+  // it could be optimized with useMemo, but that's not the case.
+  // Debouncing the search input would also be a good idea, use-debounce is a good library for that
+  const filteredPokemonList = pokemonList
+    .filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(query.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (isSortedAsc) return a.name.localeCompare(b.name);
+      return 0; // Not sorted
+    });
 
   return (
     <div className="main-content">
@@ -56,9 +69,15 @@ const PokemonList: React.FC = () => {
           onChange={handleSearch}
         />
       </div>
-      <button type="button" className="sort-button" onClick={orderByAlph}>
-        Order
-      </button>
+      <input
+        type="checkbox"
+        checked={isSortedAsc}
+        onChange={handleSort}
+        className="sort-button"
+      />
+      {/* I miss a loading state or a "no results" message
+        when there are no results. I also don't see what happens
+        if the API call fails, the user is not informed */}
       <ul className="pokemon-list">
         {filteredPokemonList.map((pokemon) => (
           <li key={pokemon.name}>{pokemon.name}</li>
